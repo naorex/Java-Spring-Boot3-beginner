eca6942b3be18d30785c51ef20bcb902d9c80962
naorex <nao23r@gmail.com>
2026-01-06 12:47:48 +0000

    UPDATE : test for query parameter

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // メソッドで処理した結果をそのままレスポンスとしてブラウザへ送信
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
   @GetMapping("/hello") // GETリクエストを処理するメソッド（ハンドラーメソッド）を意味
-  public String sayHello() {
-    return "こんにちは！";
+  public String sayHello(@RequestParam("name") String name, @RequestParam("age") int age) {
+    return "Hello, World!" + "こんにちは" + name + ", 年齢" + age + "さん！";
   }
 }
