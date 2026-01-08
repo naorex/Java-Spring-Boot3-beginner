c36f25b3005b2c3ac360de842cfa22ba0bc6a817
naorex <nao23r@gmail.com>
2026-01-08 05:43:48 +0000

    UPDATE : Hello3, Hello4Controller created

---
+++ demo/src/main/java/com/example/demo/Hello3Controller.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello3Controller {
+
+  @GetMapping("/hello3/{name}/{age}")
+  public String sayHello(@PathVariable("name") String name, @PathVariable("age") int age) {
+    return "Hello, World!" + "こんにちは" + name + "," + age + "さん！";
+  }
+}
