d72e6757adc7005040789cc844b4579ae3543157
naorex <nao23r@gmail.com>
2026-01-21 12:56:48 +0000

    UPDATE : today's progress

--- todolist/src/main/java/com/example/todolist/service/TodoService.java
+++ todolist/src/main/java/com/example/todolist/service/TodoService.java
@@ -1,11 +1,16 @@
 package com.example.todolist.service;
 
+import java.util.List;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import com.example.todolist.form.TodoData;
+import com.example.todolist.form.TodoQuery;
+import com.example.todolist.entity.Todo;
+import com.example.todolist.repository.TodoRepository;
+import com.example.todolist.common.Utils;
 
 @Service
 public class TodoService {
@@ -60,5 +65,79 @@ public class TodoService {
       }
     }
     return ans;
+  }
+
+  public boolean isValid(
+      TodoQuery todoQuery,
+      BindingResult result) {
+    boolean ans = true;
+
+    // 期限：開始の形式をチェック
+    String date = todoQuery.getDeadlineFrom();
+    if (!date.equals("")) {
+      try {
+        LocalDate.parse(date);
+      } catch (DateTimeException e) {
+        // parse できない場合
+        FieldError fieldError = new FieldError(
+            result.getObjectName(),
+            "deadlineFrom",
+            "期限：開始を入力するときは yyyy-mm-dd 形式で入力してください");
+        result.addError(fieldError);
+        ans = false;
+      }
+    }
+    // 期限：終了の形式をチェック
+    date = todoQuery.getDeadlineTo();
+    if (!date.equals("")) {
+      try {
+        LocalDate.parse(date);
+      } catch (DateTimeException e) {
+        // parse できない場合
+        FieldError fieldError = new FieldError(
+            result.getObjectName(),
+            "deadlineTo",
+            "期限：開始を入力するときは yyyy-mm-dd 形式で入力してください");
+        result.addError(fieldError);
+        ans = false;
+      }
+    }
+    return ans;
+  }
+
+  private TodoRepository todoRepository;
+
+  public List<Todo> doQuery(TodoQuery todoQuery) {
+    List<Todo> todoList = null;
+    if (todoQuery.getTitle().length() > 0) {
+      // タイトルで検索
+      todoList = todoRepository.findByTitleLike("%" + todoQuery.getTitle() + "%");
+    } else if (todoQuery.getImportance() != null && todoQuery.getImportance() != -1) {
+      // 重要度で検索
+      todoList = todoRepository.findByImportance(todoQuery.getImportance());
+    } else if (todoQuery.getUrgency() != null && todoQuery.getUrgency() != -1) {
+      // 緊急度で検索
+      todoList = todoRepository.findByUrgency(todoQuery.getUrgency());
+    } else if (!todoQuery.getDeadlineFrom().equals("") && todoQuery.getDeadlineTo().equals("")) {
+      // 期限 開始～
+      todoList = todoRepository
+          .findByDeadlineGreaterThanEqualOrderByDeadlineAsc(Utils.str2date(todoQuery.getDeadlineFrom()));
+    } else if (todoQuery.getDeadlineFrom().equals("") && !todoQuery.getDeadlineTo().equals("")) {
+      // 期限 ～終了
+      todoList = todoRepository
+          .findByDeadlineLessThanEqualOrderByDeadlineAsc(Utils.str2date(todoQuery.getDeadlineTo()));
+    } else if (!todoQuery.getDeadlineFrom().equals("") && !todoQuery.getDeadlineTo().equals("")) {
+      // 期限 開始～終了
+      todoList = todoRepository.findByDeadlineBetweenOrderByDeadlineAsc(
+          Utils.str2date(todoQuery.getDeadlineFrom()),
+          Utils.str2date(todoQuery.getDeadlineTo()));
+    } else if (todoQuery.getDone() != null && todoQuery.getDone().equals("Y")) {
+      // 完了で検索
+      todoList = todoRepository.findByDone("Y");
+    } else {
+      // 入力条件が無ければ全件検索
+      todoList = todoRepository.findAll();
+    }
+    return todoList;
   }
 }
